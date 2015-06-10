a834dec00bdcfc25b0da697ddd0a5e7a67f045d7
jmshnds <jmshnds@gmail.com>
2015-06-09 18:08:23 -0700

    comment on top

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+
+/*
+    CIS 22C Team 4 Project: Dijkstra's algorithm (shortest path)
+
+    9 June 2015
+*/
+
 //  Created by Frank M. Carrano and Tim Henry.
 //  Copyright (c) 2013 __Pearson Education__. All rights reserved.
 
@@ -28,7 +35,7 @@ void graphTest(LinkedGraph<string>* testGraph)
    string L("L");
    string M("M");
    string N("N");
-   
+
    testGraph->add(A, B, 0);
    testGraph->add(A, C, 0);
    testGraph->add(A, D, 0);
@@ -37,19 +44,19 @@ void graphTest(LinkedGraph<string>* testGraph)
    testGraph->add(C, G, 0);
    testGraph->add(D, H, 0);
    testGraph->add(D, I, 0);
-   testGraph->add(F, J, 0);   
+   testGraph->add(F, J, 0);
    testGraph->add(G, K, 0);
-   testGraph->add(G, L, 0);   
+   testGraph->add(G, L, 0);
    testGraph->add(H, M, 0);
    testGraph->add(I, M, 0);
    testGraph->add(I, N, 0);
-   
+
    cout << "Vertices :" << testGraph->getNumVertices() << endl;
    cout << "Edges :" << testGraph->getNumEdges() << endl;
-  
+
    cout << "\nDepth-first traversal (should be A B E F J C G K L D H M I N):" << endl;
    testGraph->depthFirstTraversal(A, display);
-  
+
    cout << "\nBreadth-first traversal (should be A B C D E F G H I J K L M N):" << endl;
    testGraph->breadthFirstTraversal(A, display);
 }  // end graphTest
@@ -57,19 +64,19 @@ void graphTest(LinkedGraph<string>* testGraph)
 int main()
 {
    LinkedGraph<string>* myGraph = new LinkedGraph<string>();
- 
+
    cout << "Testing Graph . . . ." << endl << endl ;
    graphTest(myGraph);
-   
+
    return 0;
 }  // end main
 
 /*
  Testing Graph . . . .
- 
+
  Vertices :14
  Edges :14
- 
+
  Depth-first traversal (should be A B E F J C G K L D H M I N):
  Displaying item - A
  Displaying item - B
@@ -85,7 +92,7 @@ int main()
  Displaying item - M
  Displaying item - I
  Displaying item - N
- 
+
  Breadth-first traversal (should be A B C D E F G H I J K L M N):
  Displaying item - A
  Displaying item - B
