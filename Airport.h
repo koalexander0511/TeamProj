e886ad947a8f80912e365e2a753bc493f5ab3aa2
Ko Outlaw-Spruell <koalexander0511@gmail.com>
2015-06-12 14:03:36 -0700

    Added operator== to Airport

--- Airport.h
+++ Airport.h
@@ -16,7 +16,9 @@ public:
 	void setAirport(string str)					{ airport = str; }
 	void setCity(string str)					{ city = str; }
 	operator=(Airport& left, Airport& right)	{ left.setAirport(right.getAirport); left.setCity(right.getCity); }
+	bool operator==(const Airport& left, const Aiport& right);
 	friend ostream & operator<<(ostream & os, const Airport &right);
+
 };
 
 ostream & operator<<(ostream & os, const Airport &right)
@@ -24,5 +26,11 @@ ostream & operator<<(ostream & os, const Airport &right)
 	os << right.airport << " " << right.city << endl;
 	return os;
 }
-
+bool Airport::operator==(const Airport& left, const Aiport& right)
+{
+	if (left.getAirport() == right.getAirport() && left.getCity() == right.getCity())
+		return true;
+	else
+		return false;
+}
 #endif
