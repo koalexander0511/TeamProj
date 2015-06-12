090a843bee90613f281b3ae341c2c69d8e58abb4
Ko Outlaw-Spruell <koalexander0511@gmail.com>
2015-06-12 13:48:34 -0700

    AirportAndInputCommit

---
+++ Airport.h
@@ -0,0 +1,28 @@
+#ifndef AIRPORT_H
+#define AIRPORT_H
+#include <string>
+#include <iostream>
+class Airport
+{
+private:
+	string airport;
+	string city;
+public:
+	Airport()									{ airport = "???"; city = "Unknown_City"; }
+	Airport(string a, string c)					{ airpot = a; city = c; }
+	Airport(const Airport& airport)				{ airport = airport.getAirport(); city = airport.getCity(); }
+	string getAirport()							{ return airport; }
+	string getCity()							{ return city; }
+	void setAirport(string str)					{ airport = str; }
+	void setCity(string str)					{ city = str; }
+	operator=(Airport& left, Airport& right)	{ left.setAirport(right.getAirport); left.setCity(right.getCity); }
+	friend ostream & operator<<(ostream & os, const Airport &right);
+};
+
+ostream & operator<<(ostream & os, const Airport &right)
+{
+	os << right.airport << " " << right.city << endl;
+	return os;
+}
+
+#endif
