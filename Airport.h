22f4860a8e39ccc615b8d518536381fcdae2b986
jmshnds <jmshnds@gmail.com>
2015-06-15 17:56:54 -0700

    airport fixes

--- Airport.h
+++ Airport.h
@@ -9,14 +9,14 @@ private:
 	string city;
 public:
 	Airport()									{ airport = "???"; city = "Unknown_City"; }
-	Airport(string a, string c)					{ airpot = a; city = c; }
-	Airport(const Airport& airport)				{ airport = airport.getAirport(); city = airport.getCity(); }
-	string getAirport()							{ return airport; }
-	string getCity()							{ return city; }
+	Airport(string a, string c)					{ airport = a; city = c; }
+	Airport(const Airport& airport)				{ this->airport = airport.getAirport(); this->city = airport.getCity(); }
+	string getAirport()	const					{ return airport; }
+	string getCity() const						{ return city; }
 	void setAirport(string str)					{ airport = str; }
 	void setCity(string str)					{ city = str; }
-	operator=(Airport& left, Airport& right)	{ left.setAirport(right.getAirport); left.setCity(right.getCity); }
-	bool operator==(const Airport& left, const Aiport& right);
+	Airport& operator=(Airport& right)	{ setAirport(right.getAirport()); setCity(right.getCity()); }
+	bool operator==(const Airport& right);
 	friend ostream & operator<<(ostream & os, const Airport &right);
 
 };
@@ -26,9 +26,10 @@ ostream & operator<<(ostream & os, const Airport &right)
 	os << right.airport << " " << right.city << endl;
 	return os;
 }
-bool Airport::operator==(const Airport& left, const Aiport& right)
+
+bool Airport::operator==(const Airport& right)
 {
-	if (left.getAirport() == right.getAirport() && left.getCity() == right.getCity())
+	if (getAirport() == right.getAirport() && getCity() == right.getCity())
 		return true;
 	else
 		return false;
