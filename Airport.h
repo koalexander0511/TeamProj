24e64f296bbac60059fdce65ad5f10919a79ede8
Ko Outlaw-Spruell <koalexander0511@gmail.com>
2015-06-15 20:44:51 -0700

    bugfixes

--- Airport.h
+++ Airport.h
@@ -15,25 +15,24 @@ public:
 	string getCity() const						{ return city; }
 	void setAirport(string str)					{ airport = str; }
 	void setCity(string str)					{ city = str; }
-	Airport& operator=(Airport& right);
-	bool operator==(const Airport& right);
+	Airport& operator=(const Airport& right);
+	bool operator==(const Airport& right) const;
 
 	friend ostream & operator<<(ostream & os, const Airport &right);
 };
 
 ostream & operator<<(ostream & os, const Airport &right)
 {
-	os << right.airport << " " << right.city << endl;
+	os << right.getAirport() << " " << right.getCity() << endl;
 	return os;
 }
-
-Airport& Airport::operator=(Airport& right)
+Airport& Airport::operator=(const Airport& right)
 {
 	setAirport(right.getAirport());
 	setCity(right.getCity());
-	return right;
+	return *this;
 }
-bool Airport::operator==(const Airport& right)
+bool Airport::operator==(const Airport& right) const
 {
 	if (airport == right.getAirport() && city == right.getCity())
 		return true;
