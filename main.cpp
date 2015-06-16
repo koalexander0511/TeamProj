d06ff02eb691f9a6c24dc82a8ddfacad948d9f60
jmshnds <jmshnds@gmail.com>
2015-06-16 00:51:30 -0700

    added to check for memory leak, works in vs
    I'm not sure where the memory leaks are coming from, and I haven't
    tested very thoroughly yet

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-// CIS 22C Team 4 Project: Dijkstra's algorithm (shortest path)
-// Program Edited by Ko Outlaw-Spruell, James Hinds, and Victoriia Petrusha
+
+/*
+	CIS 22C Team 4 Project: Dijkstra's algorithm (shortest path)
+	Program Edited by Ko Outlaw-Spruell, James Hinds, and Victoriia Petrusha
+
+	16 June 2015
+*/
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include "LinkedGraph.h"
+// #include <crtdbg.h>
+// #include "LinkedGraph.h"
 #include "Dijkstra.h"
 #include "Airport.h"
 
@@ -12,9 +19,7 @@ using namespace std;
 typedef Dijkstra<Airport> *T;
 
 void displayMenu();
-void createGraph(T &t);
 void displayAirport(Airport& port);
-
 
 bool openInputFile(ifstream &ifs) //function that asks the user to enter a file name. Opens the file, and returns true if it worked.
 {
@@ -25,6 +30,7 @@ bool openInputFile(ifstream &ifs) //function that asks the user to enter a file 
 	ifs.open(filename.c_str());
 	return ifs.is_open();
 }
+
 bool openOutputFile(ofstream &fout) //function that asks the user to enter a file name. Opens the file, and returns true if it worked.
 {
 	string filename; // temp storage for user input. Stores the name of the file that the user wants to open
@@ -34,29 +40,34 @@ bool openOutputFile(ofstream &fout) //function that asks the user to enter a fil
 	fout.open(filename.c_str());
 	return fout.is_open();
 }
+
 void createGraphFromFile(ifstream &fin, T &g)
 {
-	Airport *airport1, *airport2;
+	Airport airport1, airport2;
 	string tempPort, tempCity;
 	int dist;
 
 	while (fin >> tempPort)
 	{
 		fin >> tempCity;
-		airport1 = new Airport(tempPort, tempCity);
+		airport1.setAirport(tempPort);
+		airport1.setCity(tempCity);
 		fin >> tempPort;
 		fin >> tempCity;
-		airport2 = new Airport(tempPort, tempCity);
+		airport2.setAirport(tempPort);
+		airport2.setCity(tempCity);
 		fin >> dist;
-		g->add(*airport1, *airport2, dist);
+		g->add(airport1, airport2, dist);
 	}
 }
 
 int main()
 {
 	T g1 = new Dijkstra<Airport>();
+
 	bool done = false;
 	int choice;
+
 	string sCity, eCity;
 	string sAirport, eAirport;
 	int weight;
@@ -67,14 +78,12 @@ int main()
 	ofstream fout;
 	ifstream fin;
 
-	if(openInputFile(fin))
-        createGraphFromFile(fin, g1);
+	/*
+	// reading graph from file should be selected as a menu option
+	while(openInputFile(fin));
+    createGraphFromFile(fin, g1);
 	fin.close();
-
-	string fname;
-
-	string tempPort, tempCity;
-	Airport *port1, *port2;
+	*/
 
 	do {
 		displayMenu();
@@ -91,7 +100,7 @@ int main()
 			cin >> sAirport >> sCity;
 			cout << "Enter the end vertex(airport city): ";
 			cin >> eAirport >> eCity;
-			cout << "Enter the first weight: ";
+			cout << "Enter the weight: ";
 			cin >> weight;
 
 			tempAirport1.setAirport(sAirport);
@@ -101,6 +110,7 @@ int main()
 
 			g1->add(tempAirport1, tempAirport2, weight);
 			break;
+
 		case 3: //display the graph depth traversal
 			// Enter starting point for traversal
 			cout << "Enter the start vertex(airport city): ";
@@ -108,8 +118,10 @@ int main()
 			tempAirport1.setAirport(sAirport);
 			tempAirport1.setCity(sCity);
 
+			cout << endl;
 			g1->depthFirstTraversal(tempAirport1, displayAirport);
 			break;
+
 		case 4: //display the graph breadth traversal
 			// Enter starting point for traversal
 			cout << "Enter the start vertex(airport city): ";
@@ -117,27 +129,34 @@ int main()
 			tempAirport1.setAirport(sAirport);
 			tempAirport1.setCity(sCity);
 
+			cout << endl;
 			g1->breadthFirstTraversal(tempAirport1, displayAirport);
 			break;
+
 		case 5: //remove an edge
 
 			cout << "Enter the starting Airport(3 capital letters): ";
-			cin >> tempPort;
+			cin >> sAirport;
 			cout << "Enter the starting City:";
-			getline(cin, tempCity);
-			port1 = new Airport(tempPort, tempCity);
+			getline(cin, sCity);
+
 			cout << "Enter the ending Airport(3 capital letters):";
-			cin >> tempPort;
+			cin >> eAirport;
 			cout << "Enter the ending city:";
-			getline(cin, tempCity);
-			port2 = new Airport(tempPort, tempCity);
-			g1->remove(*port1, *port2);
-			delete port1;
-			delete port2;
+			getline(cin, eCity);
+
+			tempAirport1.setAirport(sAirport);
+			tempAirport1.setCity(sCity);
+			tempAirport2.setAirport(eAirport);
+			tempAirport2.setCity(eCity);
+
+			g1->remove(tempAirport1, tempAirport2);
 			break;
+
 		case 6: //get the number of vertices
 			cout << "The number of vertices in the graph = " << g1->getNumVertices() << endl;
 			break;
+
 		case 7: //search for a certain vertex
 		{
 			cout << "Enter a vertex(airport city): ";
@@ -153,7 +172,6 @@ int main()
 			cout << endl;
 			break;
 		}
-
 
 		case 8: //find the shorted path between two vertices
 		{
@@ -175,8 +193,9 @@ int main()
 			else{
 				g1->setStartPoint(tempAirport1);
                 g1->setStartPoint(tempAirport1);
-				g1->distanceTo(tempAirport2);
-				// g1->printShortestPath(); // add this function to dijkstra?
+				int d = g1->distanceTo(tempAirport2);
+				cout << "Distance to: " << tempAirport2 << " = " << d << endl;
+				// g1->printShortestPath(); // add this function to dijkstra? to print full path
 			}
 			break;
 		}
@@ -186,7 +205,7 @@ int main()
 			break;
 
 		case 10: // write to file
-			while (!openOutputFile(fout));
+			while(!openOutputFile(fout));
 			g1->writeToFile(fout);
 			fout.close();
 			break;
@@ -200,7 +219,7 @@ int main()
 			break;
 		} //end of switch
 
-
+		cout << endl;
 	} while (!done);
 
 	cout << "Done with testing!" << endl;
@@ -208,18 +227,23 @@ int main()
 	cin.get();
 
 	delete g1;
+
+	// test for memory leaks
+	// cout << (_CrtDumpMemoryLeaks() ? "Memory Leak\n" : "No Leak\n");
+
 	return 0;
 }
+
 // display menu
 void displayMenu()
 {
 
-	int order = 0;
+	int order = 1;
 
 	cout << order++ << ": Show the menu." << endl;
 	cout << order++ << ": Add to Graph." << endl;
-	cout << order++ << ": Display vertices." << endl;
-	cout << order++ << ": Display the graph." << endl;
+	cout << order++ << ": Display graph depth traversal." << endl;
+	cout << order++ << ": Display graph breadth traversal." << endl;
 	cout << order++ << ": Remove from graph." << endl;
 	cout << order++ << ": Get the number of vertices in the graph." << endl;
 	cout << order++ << ": Search for a certain vertex." << endl;
