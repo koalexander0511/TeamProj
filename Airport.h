d14549042767516e5e4443079d0eb4d68729f697
jmshnds <jmshnds@gmail.com>
2015-06-16 16:57:26 -0700

    small changes to functions

--- Airport.h
+++ Airport.h
@@ -13,8 +13,8 @@ private:
 	string city;
 public:
 	Airport()									{ airport = "???"; city = "Unknown_City"; }
-	Airport(string a, string c)					{ airport = a; city = c; }
-	Airport(const Airport& airport)				{ this->airport = airport.getAirport(); this->city = airport.getCity(); }
+	Airport(string a, string c)					{ setAirport(a); setCity(c); }
+	Airport(const Airport& aport)				{ setAirport(aport.airport); setCity(aport.city); }
 	string getAirport()	const					{ return airport; }
 	string getCity() const						{ return city; }
 	void setAirport(string str)					{ airport = str; }
